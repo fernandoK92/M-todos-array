1be7c970963c4661e5198bfc1952d167097459c9
Fernando Castro <facastro@sudamericano.edu.ecgit config --global user.email facastro@sudamericano.edu.ec>
2024-07-02 07:26:10 -0500

    metodos-arrays

--- 02-ejercicio.js
+++ 02-ejercicio.js
@@ -1,11 +1,33 @@
 'use strict';
 let asignaturas = [
-    { nombre: 'LAS TICS Y SOPORTE EN HARDWARE', nivel: 1 },
-    { nombre: 'BASE DE DATOS RELACIONALES', nivel: 2 },
-    { nombre: 'METODOLOGIAS PARA RESOLVER PROBLEMAS INFORMATICOS', nivel: 1 },
-    { nombre: 'PROGRAMACION DE INTERFASES GRAFICAS', nivel: 3 },
-    { nombre: 'PROYECTO INTEGRADOR DE SABERES: CREACION DE APLICACIONES', nivel: 3 },
-    { nombre: 'MATEMATICA COMPUTACIONAL APLICADA', nivel: 2 }
+    { nombre: 'Matematica computacional', nivel: 1 },
+    { nombre: 'Metodologías para resolver problemas informáticos', nivel: 1 },
+    { nombre: 'Base de datos relacionales', nivel: 1 },
+    { nombre: 'Las tics y soporte en hardware', nivel: 1 },
+    { nombre: 'Programación de interfaces gráficas y estructura de datos', nivel: 1 },
+    { nombre: 'Proyecto integrador de saberes: creación de aplicaciones de escritorio', nivel: 1 },
+    { nombre: 'Prácticas de servicio comunitario', nivel: 1 },
+    { nombre: 'Sistemas digitales programables', nivel: 2 },
+    { nombre: 'Sistemas de información', nivel: 2 },
+    { nombre: 'Programación orientada a objetos', nivel: 2 },
+    { nombre: 'Procesos contables', nivel: 2 },
+    { nombre: 'Gestion de base de datos', nivel: 2 },
+    { nombre: 'Proyecto integrador de saberes: creación de aplicaciones de escritorio', nivel: 2 },
+    { nombre: 'Prácticas de servicio comunitario', nivel: 2 },
+    { nombre: 'Programacion de aplicaciones para dispositivos móviles', nivel: 3 },
+    { nombre: 'Probabilidades y procesos estocásticos', nivel: 3 },
+    { nombre: 'Herramientas informáticas para el despliegue de diagramas', nivel: 3 },
+    { nombre: 'Aplicaciones tecnológicas autónomas', nivel: 3 },
+    { nombre: 'Software aplicativo', nivel: 3 },
+    { nombre: 'Proyecto integrador de saberes: creación de aplicaciones web con base a la arquitectura cliente servidor', nivel: 3 },
+    { nombre: 'Prácticas laborales', nivel: 3 },
+    { nombre: 'Comunicaciones y redes de datos', nivel: 4 },
+    { nombre: 'Sistemas operativos', nivel: 4 },
+    { nombre: 'Herramientas case', nivel: 4 },
+    { nombre: 'Derecho y seguridad informática', nivel: 4 },
+    { nombre: 'Tendencias tecnológicas', nivel: 4 },
+    { nombre: 'Desarrollo de proyectos de software', nivel: 4 },
+    { nombre: 'Prácticas laborales', nivel: 4 }
 ];
 
 function filtrarAsignaturasPorNivel(nivel) {
@@ -17,7 +39,8 @@ function filtrarAsignaturasPorNivel(nivel) {
     }
     return resultado;
 }
-let nivelBuscado = parseInt(prompt("Ingrese el nivel de la asignatura (1, 2 o 3):"));
+
+let nivelBuscado = parseInt(prompt("Ingrese el nivel de la asignatura (1, 2, 3 o 4):"));
 let asignaturasFiltradas = filtrarAsignaturasPorNivel(nivelBuscado);
 
 let mensaje = "Asignaturas del nivel " + nivelBuscado + ":\n" + asignaturasFiltradas.join("\n");
